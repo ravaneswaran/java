116868b50addebb7f39ffe7bc02a4d3734370230
ravaneswaran <ravaneswaran@gmail.com>
2025-08-27 12:52:10 +0530

    app password is changed

--- stock-market/sm-sub-systems/sm-quartz-scheduler-system/src/main/java/rave/code/quartz/job/mailer/HolidayMailerJob.java
+++ stock-market/sm-sub-systems/sm-quartz-scheduler-system/src/main/java/rave/code/quartz/job/mailer/HolidayMailerJob.java
@@ -57,7 +57,7 @@ public class HolidayMailerJob extends AbstractQuartzJob {
                 try {
                     LOGGER.log(Level.INFO, "SENDING MAIL....");
                     ElectronicMail electronicMail = new ElectronicMail();
-                    electronicMail.connect("smtp.gmail.com", "587", "ravaneswaran@gmail.com", "xxpn zrnn ijjz ohcy");
+                    electronicMail.connect("smtp.gmail.com", "587", "ravaneswaran@gmail.com", "test");
                     electronicMail.sendMail("noreply@stockmarket.com", "ravaneswaran@gmail.com", "Stock Market Holiday Remainder", mailContent);
                 } catch (MessagingException messagingException) {
                     LOGGER.log(Level.SEVERE, messagingException.getMessage(), messagingException);
