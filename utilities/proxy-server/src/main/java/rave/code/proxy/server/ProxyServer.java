6a3e01c47d282d5b847c26534b6348675ef6c1c5
ravaneswaran <ravaneswaran@gmail.com>
2025-08-31 11:31:49 +0530

    two utility modules proxy-server and sub-processor are included

---
+++ utilities/proxy-server/src/main/java/rave/code/proxy/server/ProxyServer.java
@@ -0,0 +1,10 @@
+package rave.code.proxy.server;
+
+/**
+ * Hello world!
+ */
+public class ProxyServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
