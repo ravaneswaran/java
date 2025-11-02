77a7275544c8d2e12108d0bec6687cefe1254b40
ravaneswaran <ravaneswaran@gmail.com>
2025-11-02 16:59:27 +0530

    quartz-override.properties is now in effect

--- stock-market/sm-web-systems/nse-india-springboot-webapp/src/main/java/rave/code/nse/web/properties/NSEQuartzOverrideProperties.java
+++ stock-market/sm-web-systems/nse-india-springboot-webapp/src/main/java/rave/code/nse/web/properties/NSEQuartzOverrideProperties.java
@@ -1,12 +1,11 @@
 package rave.code.nse.web.properties;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class NSEQuartzOverrideProperties extends Properties {
 
     public NSEQuartzOverrideProperties() throws IOException {
-        this.load(new FileInputStream("quartz-override.properties"));
+        this.load(this.getClass().getResourceAsStream("/quartz-override.properties"));
     }
 }
