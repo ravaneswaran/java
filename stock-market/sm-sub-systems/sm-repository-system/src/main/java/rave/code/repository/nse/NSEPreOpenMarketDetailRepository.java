a0049fc4a47aae60353cb6b5b0405ead0ea9ac4e
ravaneswaran <ravaneswaran@gmail.com>
2025-11-08 22:31:55 +0530

    trading stat popup work in progress

--- stock-market/sm-sub-systems/sm-repository-system/src/main/java/rave/code/repository/nse/NSEPreOpenMarketDetailRepository.java
+++ stock-market/sm-sub-systems/sm-repository-system/src/main/java/rave/code/repository/nse/NSEPreOpenMarketDetailRepository.java
@@ -28,35 +28,6 @@ public class NSEPreOpenMarketDetailRepository extends AbstractNSERepositoryManag
         return preOpenMarketDetailQuery;
     }
 
-    public Query getQuery4EntitiesWithCreatedDateBetween_09_00_AM_And_09_08_AM(String preOpenType) {
-
-        /*
-        * select pre_open_type, symbol, previous_close, indicative_equilibrium_price, price_change, price_percentage_change, final_price from nse_pre_open_market_detail where indicative_equilibrium_price <= 20 and final_price > 0 and price_percentage_change > 0 order by price_percentage_change;
-        * */
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String toDate = simpleDateFormat.format(new Date());
-        String startTimeAsStr = String.format("%s %s", toDate, "09:00:00");
-        String endTimeAsStr = String.format("%s %s", toDate, "09:08:00");
-
-        try {
-            simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-            Date starTime = simpleDateFormat.parse(startTimeAsStr);
-            Date endTime = simpleDateFormat.parse(endTimeAsStr);
-
-            String query = "SELECT * FROM NSEPreOpenMarketDetailEntity openMarketEntity WHERE openMarketEntity.preOpenType = :preOpenType AND openMarketEntity.createdDate >= :startTime AND openMarketEntity.createdDate <= :endTime";
-            Query preOpenMarketDetailQuery = this.getEntityManager().createQuery(query);
-            preOpenMarketDetailQuery.setParameter("preOpenType", preOpenType);
-            preOpenMarketDetailQuery.setParameter("startTime", starTime);
-            preOpenMarketDetailQuery.setParameter("endTime", endTime);
-
-            return preOpenMarketDetailQuery;
-
-        } catch (ParseException exception) {
-            return null;
-        }
-    }
-
     public List<NSEPreOpenMarketDetailEntity> findPreOpenMarketFOs(){
         return this.findPreOpenMarketDetails("FO");
     }
@@ -95,13 +66,13 @@ public class NSEPreOpenMarketDetailRepository extends AbstractNSERepositoryManag
             CriteriaQuery<NSEPreOpenMarketDetailEntity> criteriaQuery = criteriaBuilder.createQuery(NSEPreOpenMarketDetailEntity.class);
             Root<NSEPreOpenMarketDetailEntity> root = criteriaQuery.from(NSEPreOpenMarketDetailEntity.class);
             Predicate preOpenMarketTypePredicate = criteriaBuilder.equal(root.get("preOpenType"), preOpenType);
-            Predicate createdDatePredicate = criteriaBuilder.between(root.get("createdDate"), from, to);
+            //Predicate createdDatePredicate = criteriaBuilder.between(root.get("createdDate"), from, to);
             //Predicate indicativeEquilibriumPricePredicate = criteriaBuilder.lessThanOrEqualTo(root.get("indicativeEquilibriumPrice"), 20);
             //Predicate finalPricePredicate = criteriaBuilder.greaterThan(root.get("finalPrice"), 0);
             //Predicate pricePercentageChangePredicate = criteriaBuilder.greaterThan(root.get("pricePercentageChange"), 0);
-            Predicate whereClausePredicate = criteriaBuilder.and(preOpenMarketTypePredicate, createdDatePredicate);
+            //Predicate whereClausePredicate = criteriaBuilder.and(preOpenMarketTypePredicate, createdDatePredicate);
             //Predicate whereClausePredicate = criteriaBuilder.and(preOpenMarketTypePredicate, createdDatePredicate, indicativeEquilibriumPricePredicate, finalPricePredicate, pricePercentageChangePredicate);
-            criteriaQuery.select(root).where(whereClausePredicate).orderBy(criteriaBuilder.asc(root.get("pricePercentageChange")));
+            criteriaQuery.select(root).where(preOpenMarketTypePredicate).orderBy(criteriaBuilder.asc(root.get("pricePercentageChange")));
 
             return this.getEntityManager().createQuery(criteriaQuery).getResultList();
 
